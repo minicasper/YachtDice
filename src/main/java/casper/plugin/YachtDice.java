ff1f5819d1c35ecee3393243628008e4b47485f4
minicasper <sungsu2018@naver.com>
2022-03-23 21:02:45 +0900

    Initial commit

---
+++ src/main/java/casper/plugin/YachtDice.java
@@ -0,0 +1,18 @@
+package casper.plugin;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YachtDice extends JavaPlugin implements Listener
+{
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
